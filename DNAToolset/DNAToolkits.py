9106e30727c418b2fb7043df4d13290f3ea01575
Zhang Erkang <96480168+Zhang-EK@users.noreply.github.com>
2022-01-06 15:22:29 +0800

    第一课

--- DNAToolset/DNAToolkits.py
+++ DNAToolset/DNAToolkits.py
@@ -1,4 +1,6 @@
+# -*- coding: UTF-8 -*-
 # DNA toolkit file
+import collections
 
 Nucleotides = ["A","C","G","T"]
 
@@ -10,3 +12,11 @@ def validateSeq(dna_seq):
         if nuc not in Nucleotides:
             return False
     return tmpseq
+
+# 碱基计数
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
+    # return dict(collections.Counter(seq)) 同样的效果
--- DNAToolset/main.py
+++ DNAToolset/main.py
@@ -1,7 +1,16 @@
+# -*- coding: UTF-8 -*-
 # DNA tollset/code testing file
 from DNAToolkits import *
+import random
 
-rndDNAStr = "ATTTCGT"
+# 大写转化，输出结果
+#rndDNAStr = "ATTTCGttt"
+#print(validateSeq(rndDNAStr))
 
-print(validateSeq(rndDNAStr))
+# creating a random DNA sequence for testing:
+randDNAStr = ''.join([random.choice(Nucleotides)
+                    for nuc in range(50)])
+print(validateSeq(randDNAStr))
 
+# 碱基计数
+print(countNucFrequency(randDNAStr))
