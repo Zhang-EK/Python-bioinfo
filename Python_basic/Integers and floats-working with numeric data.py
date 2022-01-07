fdd898366791f984cafc26ec878666e4c6918942
Zhang Erkang <96480168+Zhang-EK@users.noreply.github.com>
2022-01-07 19:19:50 +0800

    Create Integers and floats-working with numeric data.py

---
+++ Python_basic/Integers and floats-working with numeric data.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+
+# integer 和 float
+# num = 3 
+# print(type(num))      # integer
+# num = 3.14
+# print(type(num))      # float
+
+# 计算指令
+# addition: 3 + 2
+# subtraction: 3 - 2
+# multiplication: 3 * 2
+# division: 3 / 2
+# floor division: 3 // 2      # 地板除
+# exponent: 3 ** 2      # 指数
+# modulus: 3 % 2     # 余数
+
+#######
+# num = 1 
+# num += 10             # 和 num = num + 1 一样
+# print(num) 
+########
+ 
+# print(abs(-3))        # 绝对值abs   
+# print(round(3.75, 1))     # 取整，默认取到整数，括号里第二个数字代表保留小数点后第几位    
+
+# 比较指令
+# equal: 3 == 2
+# not equal: 3 != 2
+# greater than: 3 > 2
+# less than: 3 < 2
+# greater or equal: 3 >= 2
+# less or equal: 3 <= 2
+
+########
+# num_1 = 3
+# num_2 = 2
+# print(num_1 == num_2)      # 输出ture or false
+########
+
+############
+# num_1 = '100'
+# num_2 = '200'
+# print(num_1 + num_2)     # 输出100200
+############
+# num_1 = '100'
+# num_2 = '200'
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+# print(num_2 + num_1)      # 可以简单使用int()来转化
+###########
