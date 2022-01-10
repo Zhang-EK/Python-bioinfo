21c84f044f434981338010e80c09b585bb05c52a
Zhang Erkang <96480168+Zhang-EK@users.noreply.github.com>
2022-01-10 15:45:53 +0800

    Create 6. Loops and iterations-forwhile loops.py

---
+++ Python_basic/6. Loops and iterations-forwhile loops.py
@@ -0,0 +1,35 @@
+#! python3
+# -*- coding: UTF-8 -*-
+
+nums = [1,2,3,4,5]
+###########
+# for num in nums:
+#     if num == 3:
+#         print('found!')
+#         break              # break
+#     print(num)
+#############
+# for num in nums:
+#     if num == 3:
+#         print('found!')
+#         continue             # continue
+#     print(num)
+#############
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+############
+# for i in range(1, 11):           # 从1开始到10结束
+#     print(i)
+###########
+# x = 0
+# while x < 10:
+#     print(x)
+#     x += 1                     # x小于10时终止循环
+###########
+# x = 0
+# while True:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
